6a5d3bbf34e2273598d784b993700ba049c10321
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-08 00:43:05 +0300

    project-update

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const button = document.querySelector("#button_1");
 button.addEventListener("click", reverseWord);
 
 function reverseWord(str) {
-    let text = str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
-    return text;
+    let text = str.split(' ');
+    console.log(text);
     }
 
 
