3ca673e0121af2f3f7d55d057b803bbb5d6472e8
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-08 00:54:06 +0300

    project-update

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-
-
 const input = document.querySelector("#text_input");
 const button = document.querySelector("#button_1");
 
 button.addEventListener("click", reverseWord);
 
-function reverseWord(str) {
-    let text = str.split(' ');
-    console.log(text);
+function reverseWord() {
+    let text = input.value.split(' ');
+    
+    for (let i = text.length; i >= 0; i--){
+        text_2 += text[i];
     }
-
-
-
-    document.querySelector("#reversed_text").innerText = text;
+    
+     
+    document.querySelector("#reversed_text").innerText = text_2;
 };
+
+   
