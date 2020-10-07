81698200636da2b8b8cf9226ac3e9ba7f2594283
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-08 00:57:37 +0300

    project-update

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ function reverseWord() {
     let text = input.value.split(' ');
     
     for (let i = text.length; i >= 0; i--){
-        text_2 += text[i];
+        
+        text_2 = text_2.append(text[i]);
     }
     
-     
+    console.log(text_2);
     document.querySelector("#reversed_text").innerText = text_2;
 };
 
