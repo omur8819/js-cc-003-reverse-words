8cbeef428eae9875e6de841c4f8ead20dbd15f22
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-08 01:03:31 +0300

    project-update

--- script.js
+++ script.js
@@ -29,4 +29,35 @@ var reverseWords = function(sentence){
 	
 }
 
+
+function StringUtil() {
+
+    this.reverseWords = function(str) {
+        var result = "";
+        var wordArray = str.split(" ");
+        for(var i = wordArray.length - 1; i >= 0; i--) {
+            result += wordArray[i] + " ";
+        }
+        return result.trim();
+    }
+
+}
+
+String.prototype.splitStr = function(delimiter) {
+    var stringArray = [];
+    var tempStr = "";
+    for(var i = 0; i < this.length; i++) {
+        if(this.charAt(i) === delimiter) {
+            stringArray.push(tempStr);
+            tempStr = "";
+        } else {
+            tempStr += this.charAt(i);
+        }
+    }
+    if(tempStr !== "") {
+        stringArray.push(tempStr);
+    }
+    return stringArray;
+}
+
    
