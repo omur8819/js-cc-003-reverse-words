8de13eac8ce693f281d9cfeef3d47785344b62ed
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-10 12:37:27 +0300

    last-commit

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const firstSentence = document.querySelector("#firstSentence");
 const button_1 = document.querySelector("#button_1");
 const reverseSentence = document.querySelector("#reverseSentence");
 
-
 button_1.addEventListener("click", reverseWord);
 firstSentence.addEventListener("keyup", enter);
 
 function reverseWord() {
-  if(firstSentence.value === "") {
-    alert("lütfen cümle giriniz : ")
+  if (firstSentence.value === "") {
+    alert("Please enter a sentence");
   }
   const reverse = firstSentence.value.split(" ").reverse().join(" ");
   reverseSentence.innerHTML = reverse;
+  firstSentence.focus();
+  firstSentence.value = "";
 };
 
 function enter(event) {
-  if(event.keyCode === 13){
+  if (event.keyCode===13){
+    // button_1.click(reverseWord)
     reverseWord();
   }
 };
+
+
